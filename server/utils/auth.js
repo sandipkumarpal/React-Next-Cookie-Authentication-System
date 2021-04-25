f3e8038aa7041b1bf2bcf81a5c3c5109f2d8e8fd
SandipDotball <sandip@dotball.com>
2021-04-25 17:14:50 +0530

    creates  password via  bcrypt

---
+++ server/utils/auth.js
@@ -0,0 +1,25 @@
+import bcrypt, { hash } from 'bcrypt'
+
+const saltRounds = 12;
+
+// Generate a salt and hash
+export const hasPassword = (password) => {
+    return new Promise((resolve, reject) => {
+        bcrypt.genSalt(saltRounds, (err, salt) => {
+            if (err) {
+                reject(err);
+            }
+            // Store hash in your password DB.
+            bcrypt.hash(password, salt, (err, hash) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(hash);
+            })
+        })
+    })
+}
+
+export const comparePassword = (password, hasedPassword) => {
+    return bcrypt.compare(password, hasedPassword)
+}
