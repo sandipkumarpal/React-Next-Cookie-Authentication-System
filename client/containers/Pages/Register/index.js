c99178234ab1aabcfd0db5ebd51e59a5150ab982
SandipDotball <sandip@dotball.com>
2021-04-26 18:02:30 +0530

    adds active menu

--- client/containers/Pages/Register/index.js
+++ client/containers/Pages/Register/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Typography } from 'antd';
 import withPageTitle from '../../../hoc/withPageTitle'
 import RegisterForm from '../../Register/Form'
+import Link from 'next/link'
+
+const { Text } = Typography;
 
 function RegisterPage() {
     return (
         <div style={{ maxWidth: 500, margin: '0 auto'}}>
             <RegisterForm />
+            <p style={{textAlign: 'center'}}>
+                <Text type="secondary" style={{textAlign: 'center'}}>Already registered? <Link href="/login"><a>Login</a></Link></Text>
+            </p>
         </div>
     )
 }
--- client/containers/TopMenu/index.js
+++ client/containers/TopMenu/index.js
@@ -1,20 +1,29 @@
 import React from 'react'
 import { Menu } from 'antd';
+import { useRouter } from 'next/router'
 import TopMenuItem from '../../components/TopMenuItem';
 import { ROUTER_PATH } from '../../config/routers'
 
 const MENUS = Object.keys(ROUTER_PATH)
 
 function TopMenu() {
+    const router = useRouter()
+    const { asPath } = router;
+
     const renderMenus = (item) => {
         const { href, IconComponent } = ROUTER_PATH[item]
         return (
-            <TopMenuItem label={item} key={item} href={href} icon={<IconComponent />} />
+            <TopMenuItem
+                label={item}
+                key={href}
+                href={href}
+                icon={<IconComponent />}
+            />
         )
     }
-    
+
     return (
-        <Menu mode="horizontal" theme="dark">
+        <Menu mode="horizontal" theme="dark" selectedKeys={[asPath]}>
             {MENUS.map(item => renderMenus(item))}
         </Menu>
     )
